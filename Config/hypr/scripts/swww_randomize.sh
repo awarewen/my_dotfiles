6200c6daad302495280ea0e11c5e099bd9727bc1
awarewen-gpd <blacktonewascrazy@gmail.com>
2023-11-24 02:46:48 +0800

    update

--- Config/hypr/scripts/swww_randomize.sh
+++ Config/hypr/scripts/swww_randomize.sh
@@ -16,28 +16,41 @@ fi
 IMG_DIR=$1
 
 # 图片轮换时间
-INTERVAL=60
+INTERVAL=300
 
 
 # swww 切换参数值
-FPS=120
-STEP=255
-## (https://cubic-bezier.com/#.1,.9,.79,.11)
-BEZIER=.1,1,.1,.4 # .1,1,.1,.1
-TYPE=any
-DURATION=2.4
+OUTPUT=none       # 屏幕输出
+IMG_RESIZE=crop   # 图像填充 Def: crop [no,crop,fit]
+FULL_COLOR=000000 # 填充颜色 Def: 000000 , Need IMG_RESIZE=fit
+FILTER=Lanczos3   # 缩放图像时使用的滤镜 Def: Lanczos3 [Nearest, bilinear, Catmullrom, Mitchell, Lanczos3]
+TYPE=random       # 切换过渡 Def: simple [none, simple, fade, left, right, top, bottom, wipe, wave, grow, center, any, outer, random]
+STEP=255          # 过渡效果速度 ( TYPE=simple STEP=2 ), Def: 90
+DURATION=2.4      # 完成过度效果时间 (单位 s) Def: 3, 不适用于 TYPE=simple
+FPS=120           # 过渡效果帧 Def: 45
+ANGLE=45          # For TYPR=wipe/wave, Def: 45
+POS=center        # For TYPE=grow/outer, Def: center ['center', 'top', 'left', 'right', 'bottom', 'top-left', 'top-right', 'bottom-left', 'bottom-right']
+BEZIER=.1,1,.1,.4 # .1,1,.1,.1 Def: 54,0,.34,.99 , (https://cubic-bezier.com/#.1,.9,.79,.11)
+WAVE=20,20        # For TYPE=wave, Def: 20,20
 ## wipe和wave用于控制擦除的角度
-ANGLE=wipe
+
 
 # swww 切换可选参数
 swww_option() {
    local transition_args=(
-      "--transition-fps"      "$FPS"
-      "--transition-step"     "$STEP"
-      "--transition-bezier"   "$BEZIER"
+#     "--outputs"             "$OUTPUT"
+#     "--no-resize"           "no"
+      "--resize"              "$IMG_RESIZE"
+#     "--fill-color"          "$FULL_COLOR"
+      "--filter"              "$FILTER"
       "--transition-type"     "$TYPE"
+      "--transition-step"     "$STEP"
       "--transition-duration" "$DURATION"
-#     "--transition-angle"    "$ANGLE"
+      "--transition-fps"      "$FPS"
+      "--transition-angle"    "$ANGLE"
+      "--transition-pos"      "$POS"
+      "--transition-bezier"   "$BEZIER"
+      "--transition-wave"     "$WAVE"
   )
   echo "${transition_args[@]}"
 }
