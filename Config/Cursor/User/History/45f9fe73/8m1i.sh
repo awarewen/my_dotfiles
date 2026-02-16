a7b99a41a173f7d7641237c96d2c260a54978ef9
awarewen-gpd <blacktonewascrazy@gmail.com>
2026-02-16 09:18:33 +0800

    update

--- Config/Cursor/User/History/45f9fe73/8m1i.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 设置输入法环境变量
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-
-# 启动 Cursor
-exec /home/awarewen/Applications/cursor-bin_28fab4e9f16f9024a8aecb624f85f5fe.AppImage "$@" 
